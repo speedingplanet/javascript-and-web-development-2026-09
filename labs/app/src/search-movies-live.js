efd7ab506487ea41dfa726aa6c69db4a30b0f56f
John Paxton <johnpaxton@gmail.com>
2026-09-16 16:06:42 -0400

    Async movie data

--- labs/app/src/search-movies-live.js
+++ labs/app/src/search-movies-live.js
@@ -1,23 +1,14 @@
-// import { movies } from '../../../data/all-data.json' with { type: 'json' };
-
 console.log('search-movies loaded.');
 let buttonRef = document.querySelector('button');
 let textField = document.querySelector('#search-title');
-let movies = await loadMovies();
 
-buttonRef.addEventListener('click', function () {
+buttonRef.addEventListener('click', async function () {
 	let searchText = textField.value;
 	textField.value = '';
 	document.querySelector('#search-message').textContent = `You searched on "${searchText}"`;
 	console.log(`You searched on ${searchText}`);
 
-	// I want to search through movies to find titles that match searchText
-	let matchingMovies = movies.filter((movie) => {
-		// return movie.title.toLocaleLowerCase().includes(searchText.toLowerCase());
-		let search = new RegExp(searchText, 'i');
-		return search.test(movie.title);
-	});
-
+	let matchingMovies = await searchMovies('title_like', searchText);
 	let target = document.querySelector('.grid-body');
 	let rows = matchesToRows(matchingMovies);
 	target.replaceChildren(...rows);
@@ -37,9 +28,12 @@ function matchesToRows(matches) {
 	return rows;
 }
 
-async function loadMovies() {
+async function searchMovies(field, text) {
 	try {
-		let response = await fetch('http://localhost:8000/movies');
+		let params = new URLSearchParams({
+			[field]: text,
+		});
+		let response = await fetch(`http://localhost:8000/movies?${params.toString()}`);
 		if (response.ok) {
 			let movies = await response.json();
 			return movies;
