c7e1210731273e605df5e183e52b94e69f1a0d17
John Paxton <johnpaxton@gmail.com>
2026-09-16 15:03:18 -0400

    Full search interface (local)

--- labs/app/src/search-movies.js
+++ labs/app/src/search-movies.js
@@ -5,14 +5,33 @@ let buttonRef = document.querySelector('button');
 let textField = document.querySelector('#search-title');
 
 buttonRef.addEventListener('click', function () {
-	console.log(`You searched on ${textField.value}`);
+	let searchText = textField.value;
+	textField.value = '';
+	document.querySelector('#search-message').textContent = `You searched on "${searchText}"`;
+	console.log(`You searched on ${searchText}`);
 
-	// I want to search through movies to find titles that match textfield.value
+	// I want to search through movies to find titles that match searchText
 	let matchingMovies = movies.filter((movie) => {
-		// return movie.title.toLocaleLowerCase().includes(textField.value.toLowerCase());
-		let search = new RegExp(textField.value, 'i');
+		// return movie.title.toLocaleLowerCase().includes(searchText.toLowerCase());
+		let search = new RegExp(searchText, 'i');
 		return search.test(movie.title);
 	});
 
-	console.log('Matching movies:', matchingMovies);
+	let target = document.querySelector('.grid-body');
+	let rows = matchesToRows(matchingMovies);
+	target.replaceChildren(...rows);
 });
+
+function matchesToRows(matches) {
+	let fields = ['title', 'year', 'rating'];
+	let rows = [];
+	for (let match of matches) {
+		let row = document.createElement('div');
+		row.classList.add('grid-row');
+		for (let field of fields) {
+			row.insertAdjacentHTML('beforeend', `<div class="grid-cell">${match[field]}</div>`);
+		}
+		rows.push(row);
+	}
+	return rows;
+}
